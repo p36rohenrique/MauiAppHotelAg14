179ff630a2b4364baf70b4935c28fa5cca447d61
pedro <pedrohenrique.onunes@hotmail.com>
2024-11-09 14:28:51 -0300

    asduf

---
+++ MauiAppHotelTransilvania/Sobre.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotelTransilvania;
+
+public partial class Sobre : ContentPage
+{
+	public Sobre()
+	{
+		InitializeComponent();
+	}
+}
--- MauiAppHotelTransilvania/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotelTransilvania/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,15 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void Avanco(object sender, EventArgs e)
+    {
+		try
+		{
+			Navigation.PushAsync(new HospedagemContratada());
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
