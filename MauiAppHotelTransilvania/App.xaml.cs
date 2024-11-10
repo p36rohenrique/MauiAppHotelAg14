66723b050a34079130fdb73871e2453d9ea18e5b
pedro <pedrohenrique.onunes@hotmail.com>
2024-11-09 23:09:36 -0300

    final

--- MauiAppHotelTransilvania/App.xaml.cs
+++ MauiAppHotelTransilvania/App.xaml.cs
@@ -1,7 +1,37 @@
-﻿namespace MauiAppHotelTransilvania
+﻿using MauiAppHotelTransilvania.Models;
+
+namespace MauiAppHotelTransilvania
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 150.0,
+                ValorDiariaCrianca = 75.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 125.0,
+                ValorDiariaCrianca = 62.5
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.0,
+                ValorDiariaCrianca = 25.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.0,
+                ValorDiariaCrianca = 12.5
+            }
+        };
+
         public App()
         {
             InitializeComponent();
--- MauiAppHotelTransilvania/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotelTransilvania/Views/ContratacaoHospedagem.xaml.cs
@@ -2,10 +2,22 @@ namespace MauiAppHotelTransilvania.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
 {
+	App PropriedadesApp;
 	public ContratacaoHospedagem()
 	{
 		InitializeComponent();
-	}
+
+		PropriedadesApp = (App)Application.Current;
+
+		pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+		dtpck_checkin.MinimumDate = DateTime.Now;
+		dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+		dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+    }
+
     private void Avanco(object sender, EventArgs e)
     {
 		try
@@ -16,5 +28,15 @@ public partial class ContratacaoHospedagem : ContentPage
 		{
 			DisplayAlert("Ops", ex.Message, "Ok");
 		}
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+		DatePicker elemento = sender as DatePicker;
+
+		DateTime data_selecionada_checkin = elemento.Date;
+
+		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
     }
 }
